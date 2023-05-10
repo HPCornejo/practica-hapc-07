48739d1fef9791ed0ca417c5b4d4d239fe223ce9
HPCornejo <4f20perezcornejohector@gmail.com>
2023-05-09 23:46:03 -0600

    practica-hapc-07

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const hbs = require('hbs');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const puerto = process.env.PORT || 3000;
+
+const app = express();
+
+app.set('view engine','hbs');
+hbs.registerPartials(__dirname + '/views/partials',()=>{});
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/',(req,res)=>{
+    res.render('dashboard');
+});
+
+app.get('/login',(req,res)=>{
+    res.render('login');
+});
+
+app.get('/registro',(req,res)=>{
+    res.render('registro');
+});
+
+app.get('*',(req,res)=>{
+    res.render('404');
+});
+
+app.listen(puerto,()=>{
+    console.log('El servidor esta corriendo en el puerto: ',puerto);
+});
